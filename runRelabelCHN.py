0bd45ae64134796ce238926c5928fbc13060f84e
AnneSWarlaumont <anne.warlaumont@gmail.com>
2020-02-20 14:26:15 -0800

    Updated directory where the wav file is found.

--- runRelabelCHN.py
+++ runRelabelCHN.py
@@ -9,6 +9,6 @@ Updated on Thu Feb 20
 
 import relabel
 
-relabel.relabel_CHN('0274_000221_scrubbed.wav',
+relabel.relabel_CHN('../0274_000221_scrubbed.wav',
                     '0274_000221_scrubbed_CHNrelabel_warlaumont.csv',
                     '0274_000221_segments.csv')
