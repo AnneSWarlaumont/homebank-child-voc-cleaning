edeba22735adf70072459de0b7c92ac669b3268c
AnneSWarlaumont <anne.warlaumont@gmail.com>
2020-02-20 14:15:04 -0800

    Changed command so I can run with a different file.

--- runRelabelCHN.py
+++ runRelabelCHN.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb 14 13:19:16 2020
+Updated on Thu Feb 20
 
 @author: Anne S. Warlaumont
 """
 
 import relabel
 
-relabel.relabel_CHN('0833_010606.wav',
-                    '0833_010606_CHNrelabel_warlaumont.csv',
-                    '0833_010606_segments.csv')
+relabel.relabel_CHN('0274_000221_scrubbed.wav',
+                    '0274_000221_scrubbed_CHNrelabel_warlaumont.csv',
+                    '0274_000221_segments.csv')
