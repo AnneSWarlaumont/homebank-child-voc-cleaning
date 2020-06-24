9338bb3a4f963f57ffe2cb80cf132bc80a53f85c
AnneSWarlaumont <anne.warlaumont@gmail.com>
2020-06-24 15:13:10 -0700

    Adding functionality to update the status of the listener's assignments (in progress; added change to inprogress but still need to add change to finished).

--- runRelabelCHN.py
+++ runRelabelCHN.py
@@ -7,7 +7,7 @@ Updated on Thu Feb 20
 @author: Anne S. Warlaumont
 """
 
-import relabel, csv
+import relabel, csv, fileinput
 
 # Get the user to enter their ID
 # so we can look up what they should be listening to
@@ -17,6 +17,8 @@ userID = input('\nWhat is your UCLA Logon ID? ')
 # Read in the user's assignments txt file
 userAssignmentsFile = open('relabelCHN_assignments_'+userID+'.txt','r')
 userAssignmentsLines = userAssignmentsFile.readlines()
+userAssignmentsFile.close()
+
 
 # Find the first line that doesn't end in "finished" and call
 # relabel.relabel_CHN on each unfinished line.
@@ -24,6 +26,12 @@ for assignment in userAssignmentsLines[1:]:
     assignmentElements = assignment.rstrip('\n').split(',')
     status = assignmentElements[3]
     if status != 'finished':
+        if status == 'unstarted':
+            # edit the userAssignmentsFile line to make status "inprogress"
+            updatedLine = assignment.replace("unstarted","inprogress")
+            for line in fileinput.input('relabelCHN_assignments_'+userID+'.txt', inplace=True):
+                print(line.replace(assignment,updatedLine), end="")
         relabel.relabel_CHN('../'+assignmentElements[0],
                             assignmentElements[2],
                             assignmentElements[1])
+        # edit the userAssignmentsFile line to make status "finished"
